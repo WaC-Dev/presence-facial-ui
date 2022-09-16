845fec4daa81e79b180f9c4d2248494f2c6bf40d
Mitsanta_ANDRIAH <103644537+Mitsanta12@users.noreply.github.com>
2022-09-17 00:29:04 +0300

    feat: change interface login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './Componnent/Login';
+import Login from './Componnent/Login';
 
 function App() {
   return (
-    <div className="App">
-      <form className='wrap'>
-          <fieldset className='field-area'>
-            <label htmlFor="name">Name</label>
-            <input type="text"  id="name" />
-          </fieldset>
-          <fieldset className='field-area'>
-            <label htmlFor="password">Pass:</label>
-            <input type="password"  id="password" />
-          </fieldset>
-          <button type='submit'>Login</button>
-      </form>
-    </div>
+   <>
+       <Login/>
+   </>
   );
 }
 
