57cc4974835fe0d01d462ae852e3ff6436f5779f
Mitsanta_ANDRIAH <103644537+Mitsanta12@users.noreply.github.com>
2022-09-16 08:05:45 +0300

    modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='wrap'>
+          <fieldset className='field-area'>
+            <label htmlFor="name">Name</label>
+            <input type="text"  id="name" />
+          </fieldset>
+          <fieldset className='field-area'>
+            <label htmlFor="password">Pass:</label>
+            <input type="password"  id="password" />
+          </fieldset>
+          <button type='submit'>Login</button>
+      </form>
     </div>
   );
 }
